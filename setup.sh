00654064f8d6eb2c9d1080e3bdd82a096dafae52
Willie Scott <scottwe28@mounties.mansfield.edu>
2016-09-16 17:59:32 +0000

    AWS Web Server Setup Script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# by Willie Scott on 20160916
+# run as root; chmod 700 setup.sh; ./setup.sh
+
+date
+
+apt-get -qq update -y
+apt-get -qq upgrade -y
+
+hostnamectl set-hostname localhost
+hostnamectl
+
+apt-get -qq install -y zip unzip
+
+apt-get -qq install -y apache2
+systemctl restart apache2
+
+date
